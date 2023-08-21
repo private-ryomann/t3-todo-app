9c24fc8cc65650b6ebbcb2509555a6d50d185e76
private-ryomann <ryomann46497@gmail.com>
2023-08-21 12:59:08 +0900

    create todoRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+import { todoRouter } from "~/server/api/routers/todo";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,8 +7,20 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  example: todoRouter,
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
+
+// import { createTRPCRouter } from "~/server/api/trpc";
+
+// import { exampleRouter } from "~/server/api/routers/example";
+
+// // 定義したルータをサーバのプライマリルーターに渡す。
+// const appRouter = createTRPCRouter({
+//   example: exampleRouter,
+// });
+
+// // API の型定義をエクスポートする
+// export type AppRouter = typeof appRouter;
